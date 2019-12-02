216fba8213061c171bef924d6eb70eb1ff41c2fd
Fawad <fawad_12@outlook.com>
2019-12-02 11:52:38 +0500

    Add project files.

---
+++ AjaxLab/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using AjaxLab.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static List<Course> _courses = new List<Course>()
+        {
+            new Course { CourseCode="SE1391",Title="Web Application Development" }
+        };
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult GetCourses()
+        {
+            return Json(_courses);
+        }
+    }
+}
---
+++ AjaxLab/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AjaxLab.Models
+{
+    public class Course
+    {
+        [Required]
+        [RegularExpression("^[a-zA-Z]{2}[0-9]{4}")]
+        public string CourseCode { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+    }
+}
